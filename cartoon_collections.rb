71ebac16facb65a69de1f78164e925a6e5f31017
Dani Lyn <dlyn1110@gmail.com>
2018-04-12 15:35:24 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ def long_planeteer_calls(long_planeteer_calls)
 long_planeteer_calls.length > 4 ? true : false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(cheese_types)
+  cheeses = ["cheddar", "gouda", "camembert"]
+  cheese_types.find { | i | cheeses.include?(i)}
 end
