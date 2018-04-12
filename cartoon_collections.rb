5fecd863cf9bbf12ded8f7cedfc323b176e07862
Dani Lyn <dlyn1110@gmail.com>
2018-04-12 14:47:43 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,9 +7,11 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
+  planeteer_calls = []
   planeteer_calls.collect do | name | name + "!"
 end
 end
+
 
 def long_planeteer_calls# code an argument here
   # Your code here
