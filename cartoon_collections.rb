b7f8e0678c1344e52811c3e29a0d6234f3173382
Dani Lyn <dlyn1110@gmail.com>
2018-04-12 14:49:02 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls = []
-  planeteer_calls.collect do | name | name + "!"
+  planeteer_calls.capitalize do | name | name + "!"
 end
 end
 
